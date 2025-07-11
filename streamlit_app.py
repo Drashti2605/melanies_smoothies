1e48cd1daa318c4be237ec65d416ed937f3d5359
Drashti <drashtisakhareliya@gmail.com>
2025-07-11 12:20:07 +0530

    Update streamlit_app.py
    xscascascada

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,11 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
-# pd_df= my_dataframe.to_pandas()
-# st.dataframe(pd_df)
+# st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
+pd_df= my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 # my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
@@ -40,6 +40,9 @@ if ingredients_list:
   
   for each_fruit in ingredients_list:
     ingredients_string += each_fruit + ' '
+    
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
      # search_on = pd_df.loc[pd_df['FRUIT_NAME']== each_fruit,'SEARCH_ON'].iloc(0)
         # st.write('The search value for ', each_fruit,'search_on','.')
